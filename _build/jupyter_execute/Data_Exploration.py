3cdd0ecccd1fed67d409b6a76d7922b260156478
seancraven <seanpietercraven@gmail.com>
2022-06-08 21:35:36 +0100

    Chapter 0

---
+++ _build/jupyter_execute/Data_Exploration.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #  Data Exploration Keeling Curve 
+# 
+# 
+# 
+#  Development of readable maintainable code is not something often taught in an undergraduate Physics course. However, when working with others or on long projects, the veracity of 'Code is read more than it is written'-Guido Van Rossum becomes evident. Becuase of this, the author would recommend that you are accustomed to PEP-8 and PEP-484. In addition, the Google Python Style guide has some very prudent suggestions worth reading. This, in conjunction with good naming practices and reasonable amounts of comments, makes deciphering code a feasible task. Such practices outlined in these documents will help the reader and future contributors collaborate and maintain this and other work.
+# 
+# The initial author hopes that their original body of work is easily accessible and can be built off with relative ease and that his effort in making a framework for continuation bears fruit. 
+# 
